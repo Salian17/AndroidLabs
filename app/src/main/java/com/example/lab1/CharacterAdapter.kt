0ca30cf1958d35583d0d28c5ca0a95fdc5356c02
Salian17 <sazonov1744@yandex.ru>
2024-11-26 17:11:56 +0300

    lab5 add bindings in CharacterAdapter

--- app/src/main/java/com/example/lab1/CharacterAdapter.kt
+++ app/src/main/java/com/example/lab1/CharacterAdapter.kt
@@ -1,34 +1,30 @@
 package com.example.lab1
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-
+import com.example.lab1.databinding.ItemCharacterBinding
 
 class CharacterAdapter(private val characters: List<Character>) :
     RecyclerView.Adapter<CharacterAdapter.CharacterViewHolder>() {
 
-    class CharacterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val nameTextView: TextView = itemView.findViewById(R.id.character_name)
-        private val cultureTextView: TextView = itemView.findViewById(R.id.character_culture)
-        private val playedByTextView: TextView = itemView.findViewById(R.id.character_played_by)
-        private val aliasesTextView: TextView = itemView.findViewById(R.id.character_aliases)
-
+    class CharacterViewHolder(private val binding: ItemCharacterBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(character: Character) {
-            nameTextView.text = "Name: ${character.name}"
-            cultureTextView.text = "Culture: ${character.culture ?: "N/A"}"
-            aliasesTextView.text = "Aliases: ${character.aliases.joinToString(", ").ifEmpty { "N/A" }}"
-            playedByTextView.text = "Played By: ${character.playedBy.joinToString(", ").ifEmpty { "N/A" }}"
+            binding.characterName.text = "Name: ${character.name}"
+            binding.characterCulture.text = "Culture: ${character.culture ?: "N/A"}"
+            binding.characterAliases.text = "Aliases: ${character.aliases.joinToString(", ").ifEmpty { "N/A" }}"
+            binding.characterPlayedBy.text = "Played By: ${character.playedBy.joinToString(", ").ifEmpty { "N/A" }}"
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_character, parent, false)
-        return CharacterViewHolder(view)
+        val binding = ItemCharacterBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return CharacterViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
